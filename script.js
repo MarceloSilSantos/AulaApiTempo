64589f184c90cd3c09209a64569c065813863f38
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-04-25 16:58:52 -0300

    update code js and html

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const chave = '777fd6c175f16899b669ab9b22be7638'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid=${chave}&units=metric`
+function chamarPrevisao() {
 
-const obterTemp = () =>{
+  const chave = '777fd6c175f16899b669ab9b22be7638'
+  const cidade = 'São Paulo';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=${chave}&units=metric`
+
+
 
   fetch(url)
-        .then(resposta =>{
-          // console.log(resposta)
-          return resposta.json()
+        .then(response => response.json())
+        .then (data => {
+          const descricao = data.weather[0].description;
+          const temperatura = data.main.temp;
+        
+          document.getElementById('mensagem').innerHTML = 'Previsao para ${cidade}: ${descricao}.Temperatura: ${temperatura} °C';
         })
-          .then(dados =>{
-            console.log(dados)
+        
+        .catch(erro => {
+            console.error('Ocorreu erro ao buscar dados da API', erro);
           })
-          .catch(erro => {
-            console.log(erro)
-          })
- }
+ 
+ window.onload = chamarPrevisao();
+
+}       
